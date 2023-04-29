bfc43870d656eb234f6e96aa36929142ba95a5b2
Matman777 <mathias.mauer777@gmail.com>
2023-04-29 14:47:16 +0200

    created server

---
+++ src/pages/home.js
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (<><div className="home">
+        <h1>Bienvenue sur l'annuaire de HarvestCorp</h1>
+    </div>
+        <div>Entrez un nom, service ou un site pour contacter un collaborateur:</div>
+    </>)
+}
